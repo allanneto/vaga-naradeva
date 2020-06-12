bb5367d46ff8ef1c72de41c3c357d088c60c9283
Allan Neto <allan.netto@hotmail.com>
2020-06-11 23:47:32 -0300

    Pagina Finalizada

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -14,6 +14,12 @@ import {
 
 import LogoNara from '../../assets/logoNaradevaShalaTransp.png';
 import Yoga from '../../assets/ioga.jpg';
+import Astrologia from '../../assets/astrologiaVedica.jpg';
+import Yotisha from '../../assets/learnjyotisha.jpg';
+import Nutricao from '../../assets/Nutricao.jpg';
+import HomeBanner from '../../assets/BannerHome.png';
+import Sponsor1 from '../../assets/avp.png';
+import Sponsor2 from '../../assets/escola.png';
 
 import * as Styled from './styles';
 
@@ -87,7 +93,7 @@ function Home() {
             <h1>Instituto de Culta Hindu Naradeva Shala</h1>
           </Styled.WellcomePannel>
           <Styled.NewsletterBar>
-            <span>Receba Novidades do Instituto</span>
+            <span>Receba novidades do Instituto</span>
             <Styled.Input placeholder="Digite seu nome" />
             <Styled.Input placeholder="Digite seu email" />
             <Styled.Button>Receber</Styled.Button>
@@ -105,8 +111,154 @@ function Home() {
               limitadas
             </Styled.Description>
           </Styled.Article>
+          <Styled.Article>
+            <Styled.Image src={Astrologia} />
+            <Styled.Title>
+              Atendimento de Jyotisha - Astrologia Védica com Margaret Mahan
+              (USA-Canadá)
+            </Styled.Title>
+            <Styled.Description>
+              A leitura natal oferece um retrato dos karmas dominantes presentes
+              em sua vida, e lhe dará uma perspectiva para poder explorar os
+              pontos fortes e desafios do seu propósito de vida!
+            </Styled.Description>
+          </Styled.Article>
+          <Styled.Article>
+            <Styled.Image src={Yotisha} />
+            <Styled.Title>
+              Ferramentas para a Saúde e a Felicidade: Uma Introdução ao
+              Jyotisha!
+            </Styled.Title>
+            <Styled.Description>
+              O workshop está organizado de modo a apresentar ferramentas
+              básicas que lhe servirão onde quer que você se encontre na vida.
+            </Styled.Description>
+          </Styled.Article>
+          <Styled.Article>
+            <Styled.Image src={Nutricao} />
+            <Styled.Title>Consulta de Nutrição com Raissa Faro</Styled.Title>
+            <Styled.Description>
+              Raissa Faro é nutricionista, com aperfeiçoamento em Alimentação e
+              Cultura, pós-graduanda em Ayurveda pelo Instituto Naradeva Shala.
+            </Styled.Description>
+          </Styled.Article>
         </Styled.Articles>
+        <Styled.BannerHome src={HomeBanner} />
+        <Styled.Articles>
+          <Styled.Article>
+            <Styled.Image src={Yoga} />
+            <Styled.Title>
+              Formação e Aprimoramento em Yoga - São Paulo / SP
+            </Styled.Title>
+            <Styled.Description>
+              Curso Livre de Formação e Aprimoramento em Yoga e PósGraduação
+              Lato Sensu em Yoga (MEC) - Nova turma em março de 2019. Vagas
+              limitadas
+            </Styled.Description>
+          </Styled.Article>
+          <Styled.Article>
+            <Styled.Image src={Astrologia} />
+            <Styled.Title>
+              Atendimento de Jyotisha - Astrologia Védica com Margaret Mahan
+              (USA-Canadá)
+            </Styled.Title>
+            <Styled.Description>
+              A leitura natal oferece um retrato dos karmas dominantes presentes
+              em sua vida, e lhe dará uma perspectiva para poder explorar os
+              pontos fortes e desafios do seu propósito de vida!
+            </Styled.Description>
+          </Styled.Article>
+          <Styled.Article>
+            <Styled.Image src={Yotisha} />
+            <Styled.Title>
+              Ferramentas para a Saúde e a Felicidade: Uma Introdução ao
+              Jyotisha!
+            </Styled.Title>
+            <Styled.Description>
+              O workshop está organizado de modo a apresentar ferramentas
+              básicas que lhe servirão onde quer que você se encontre na vida.
+            </Styled.Description>
+          </Styled.Article>
+          <Styled.Article>
+            <Styled.Image src={Nutricao} />
+            <Styled.Title>Consulta de Nutrição com Raissa Faro</Styled.Title>
+            <Styled.Description>
+              Raissa Faro é nutricionista, com aperfeiçoamento em Alimentação e
+              Cultura, pós-graduanda em Ayurveda pelo Instituto Naradeva Shala.
+            </Styled.Description>
+          </Styled.Article>
+        </Styled.Articles>
+        <Styled.EbookBanner>
+          <div>
+            <h2>Oferecer aqui um Ebook</h2>
+            <button type="button">Baixar Ebook</button>
+          </div>
+        </Styled.EbookBanner>
+        <Styled.Box>
+          <Styled.Profile>
+            <img src="" alt="" />
+            <div>
+              <h3>Loja Online</h3>
+              <p>
+                Nós oferecemos uma gama completa de produtos de estilo de vida
+                Ayurveda. Você pode encomendar por telefone ou online
+              </p>
+              <button type="button">COMPRAR</button>
+            </div>
+          </Styled.Profile>
+          <Styled.Profile>
+            <img src="" alt="" />
+            <div>
+              <h3>Revista</h3>
+              <p>
+                Nós oferecemos uma gama completa de produtos de estilo de vida
+                Ayurveda. Você pode encomendar por telefone ou online.{' '}
+              </p>
+              <button type="button">ACESSAR</button>
+            </div>
+          </Styled.Profile>
+        </Styled.Box>
       </Styled.Content>
+      <Styled.Footer>
+        <Styled.Sponsor>
+          <img src={Sponsor1} alt="" />
+          <img src={Sponsor2} alt="" />
+        </Styled.Sponsor>
+        <Styled.SocialInfo>
+          <ul>
+            <li>
+              <strong>REDES SOCIAIS</strong>
+            </li>
+            <li>
+              <FaInstagram size={20} color="black" />
+              <span>Blog Naradeva Shala</span>
+            </li>
+            <li>
+              <FaFacebook size={20} color="black" />
+              <span>Youtube Naradeva Shala</span>
+            </li>
+            <li>
+              <FaFacebook size={20} color="black" />
+              <span>Facebook Naradeva Shala</span>
+            </li>
+            <li>
+              <FaInstagram size={20} color="black" />
+              <span>Instagram Naradeva Shala</span>
+            </li>
+          </ul>
+        </Styled.SocialInfo>
+        <Styled.About>
+          <ul>
+            <li>
+              <strong>SOBRE</strong>
+            </li>
+            <li>Quem somos</li>
+            <li>Termos de Uso</li>
+            <li>Termos de Privacidade</li>
+            <li>Propriedade Intelectual</li>
+          </ul>
+        </Styled.About>
+      </Styled.Footer>
     </Styled.Container>
   );
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 import bgHeader from '../../assets/bgHeader.png';
 import bgBanner from '../../assets/bgBanner.png';
+import adBanner from '../../assets/banner-item-one.png';
 
 export const Container = styled.div`
   display: flex;
@@ -209,19 +210,25 @@ export const Button = styled.button`
 `;
 
 export const Articles = styled.div`
+  margin-top: 40px;
   padding: 0px 200px;
 
   @media (max-width: 1200px) {
     padding: 0px 100px;
+    grid-template-columns: 1fr 1fr;
+    gap: 20px;
   }
 
   display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  gap: 20px;
 `;
 
 export const Article = styled.div`
   display: flex;
-  max-width: 300px;
+  max-width: 350px;
   flex-direction: column;
+  box-shadow: 0 2px 15px rgba(0, 0, 0, 0.4);
 `;
 
 export const Image = styled.img`
@@ -229,11 +236,187 @@ export const Image = styled.img`
 `;
 
 export const Title = styled.h3`
+  padding: 10px;
   font-weight: bold;
   text-align: center;
   margin: 10px 0;
 `;
 
 export const Description = styled.p`
+  padding: 10px;
   font-size: 18px;
+`;
+
+export const BannerHome = styled.img`
+  margin: 50px 200px;
+  border: 1px solid #ccc;
+`;
+
+export const EbookBanner = styled.div`
+  margin: 50px 200px;
+  height: 400px;
+  background: url(${adBanner}) no-repeat;
+  background-size: cover;
+  background-position: center;
+  display: flex;
+  flex-direction: row-reverse;
+
+  @media (max-width: 1200px) {
+    margin: 50px 100px;
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 50px 200px;
+
+    @media (max-width: 1200px) {
+      margin: 50px 100px;
+    }
+
+    h2 {
+      margin-bottom: 20px;
+    }
+
+    button {
+      height: 50px;
+      width: 300px;
+      border-radius: 4px;
+      border: 0;
+
+      color: #fff;
+      font-size: 18px;
+      font-weight: bold;
+      background: #df6116;
+
+      box-shadow: 1px 1px 1px 1px rgba(0, 0, 0, 0.4);
+    }
+  }
+`;
+
+export const Box = styled.div`
+  display: flex;
+  padding: 0 200px;
+  align-items: center;
+  justify-content: space-between;
+
+  @media (max-width: 1200px) {
+    padding: 0 100px;
+  }
+`;
+
+export const Profile = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  img {
+    height: 200px;
+    width: 200px;
+    border: 1px solid #000;
+    background: #ccc;
+    border-radius: 50%;
+  }
+
+  div {
+    display: flex;
+    max-width: 500px;
+
+    margin-left: 20px;
+
+    align-items: flex-start;
+    flex-direction: column;
+
+    h3 {
+      color: #9b1d31;
+    }
+
+    p {
+      padding: 20px 0px;
+      font-size: 18px;
+    }
+    button {
+      display: flex;
+      align-items: center;
+
+      padding: 10px 20px;
+
+      border-radius: 4px;
+      border: 0;
+
+      color: #fff;
+      font-size: 18px;
+      font-weight: bold;
+      background: #df6116;
+
+      box-shadow: 1px 1px 1px 1px rgba(0, 0, 0, 0.4);
+    }
+  }
+`;
+
+export const Footer = styled.div`
+  display: flex;
+  background: #eeeeee;
+  margin-top: 40px;
+
+  padding: 50px 200px;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const Sponsor = styled.div`
+  display: flex;
+  align-items: center;
+  img {
+    margin-right: 20px;
+  }
+`;
+
+export const SocialInfo = styled.div`
+  strong {
+    font-size: 20px;
+  }
+
+  li {
+    display: flex;
+    align-items: center;
+    margin-bottom: 5px;
+
+    cursor: pointer;
+
+    font-size: 20px;
+
+    svg {
+      margin-right: 5px;
+    }
+
+    &:first-child {
+      margin-bottom: 20px;
+    }
+  }
+`;
+
+export const About = styled.div`
+  strong {
+    font-size: 20px;
+  }
+
+  li {
+    display: flex;
+    align-items: center;
+    margin-bottom: 5px;
+
+    cursor: pointer;
+
+    font-size: 20px;
+
+    svg {
+      margin-right: 5px;
+    }
+
+    &:first-child {
+      margin-bottom: 20px;
+    }
+  }
 `;
